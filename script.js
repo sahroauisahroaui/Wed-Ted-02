ece82d42a092aece622d73fb033456522768a2f0
sahroauisahroaui <shwshwshyma189@gmail.com>
2026-04-24 09:42:36 +0100

    Refactor event handling and error messages in script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-    $('#addCourse').click(function () {
+    $('#addCourse').click(function (e) {
+        e.preventDefault();
         var row = $('.course-row').first().clone();
         row.find('input').val('');
+        row.find('.remove-row').parent().remove();
         row.append('<div class="col-auto"><button type="button" class="btn btn-danger remove-row">×</button></div>');
         $('#courses').append(row);
     });
@@ -12,17 +14,20 @@ $(document).ready(function () {
         }
     });
 
-    $('#gpaForm').submit(function (e) {
+    $('#gpaForm').on('submit', function (e) {
         e.preventDefault();
-        
+
         $.ajax({
             url: 'calculate.php',
             type: 'POST',
             data: $(this).serialize(),
             dataType: 'json',
+            beforeSend: function() {
+                $('#result').html('<div class="alert alert-info">Calculating...</div>');
+            },
             success: function (response) {
-                let alertClass = 'alert-info';
                 if (response.success) {
+                    let alertClass = 'alert-info';
                     if (response.gpa >= 3.7) alertClass = 'alert-success';
                     else if (response.gpa >= 3.0) alertClass = 'alert-info';
                     else if (response.gpa >= 2.0) alertClass = 'alert-warning';
@@ -30,15 +35,15 @@ $(document).ready(function () {
 
                     $('#result').html(
                         '<div class="alert ' + alertClass + '">' + response.message + '</div>' + 
-                        response.progressBar + 
-                        response.tableHtml
+                        (response.progressBar || '') + 
+                        (response.tableHtml || '')
                     );
                 } else {
                     $('#result').html('<div class="alert alert-danger">' + response.message + '</div>');
                 }
             },
             error: function () {
-                $('#result').html('<div class="alert alert-danger">Server Error</div>');
+                $('#result').html('<div class="alert alert-danger">Error: Could not connect to calculate.php</div>');
             }
         });
     });
